005ffb369ca2822ff4d419b3c3fba45b93a408c7
Naina Mohamed U <99238720+naina2801@users.noreply.github.com>
2023-06-21 20:01:09 +0530

    Add files via upload

---
+++ roller_coster_game.py
@@ -0,0 +1,6 @@
+print("welcome to the roller coaster")
+height = int(input("please enter your height : "))
+if height >= 120:
+    print("you can ride the roller coaster")
+else:
+    print("You can't ride the roller coaster")
